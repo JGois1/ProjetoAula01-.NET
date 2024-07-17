ba6def801fec917f1fca0d1341b6e2e7d1a4ab29
João Gois de Otoni <127162053+JGois1@users.noreply.github.com>
2024-07-16 22:02:50 -0300

    Add files via upload

---
+++ ProjetoAula01/ProjetoAula01/Program.cs
@@ -0,0 +1,56 @@
+﻿//declarando a localização da classe dentro do projeto
+using ProjetoAula01.Entidades;
+using ProjetoAula01.Repositorios;
+
+namespace ProjetoAula01
+{
+    //definindo o nome da classe
+    public class Program
+    {
+        //método para execução da classe, esse é o metodo/rotina 
+        //utilizando para executar o projeto.
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\nSISTEMA PARA CADASTRO DE CLIENTES:\n");
+
+            var cliente = new Cliente();
+
+            //set:
+
+            cliente.Id = Guid.NewGuid();
+            cliente.DataInclusao = DateTime.Now;
+
+            Console.Write("INFORME O NOME DO CLIENTE:");
+            cliente.Nome = Console.ReadLine();
+
+            Console.Write("INFORME O CPF DO CLIENTE:");
+            cliente.Cpf = Console.ReadLine();
+
+            Console.Write("INFORME O EMAIL DO CLIENTE:");
+            cliente.Email = Console.ReadLine();
+
+            Console.Write("INFORME O TELEFONE DO CLIENTE:");
+            cliente.Telefone = Console.ReadLine();
+            
+            //get:
+
+            Console.WriteLine("\nDADOS DO CLIENTE:");
+            Console.WriteLine("\tID              :" + cliente.Id);
+            Console.WriteLine("\tNome            :" + cliente.Nome);
+            Console.WriteLine("\tEmail           :" + cliente.Email);
+            Console.WriteLine("\tCpf             :" + cliente.Cpf);
+            Console.WriteLine("\tTelefone        :" + cliente.Telefone);
+            Console.WriteLine("\tIncluido em     :" + cliente.DataInclusao);
+
+            //criando um objeto / variavel de instancia para ClienteRepositorio
+            var clienteRepositorio = new ClienteRepositorio();
+
+            //exportar os dados do cliente para arquivo
+            clienteRepositorio.ExportarDados(cliente);
+
+            //imprimindo mensagem..
+            Console.WriteLine("\nDados gravados com sucesso");
+
+        }
+    }
+}
---
+++ ProjetoAula01/ProjetoAula01/Repositorios/ClienteRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using ProjetoAula01.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAula01.Repositorios
+{
+    public class ClienteRepositorio
+    {
+        /// <summary>
+        /// Rotina/metodo pra gravar dados do cliente em txt
+        /// </summary>
+        /// <param name="cliente">Objeto contendo os dados da classe Cliente</param>
+        public void ExportarDados(Cliente cliente)
+        {
+            var streamWriter = new StreamWriter("c:\\temp\\clientes.txt", true);
+
+            streamWriter.WriteLine("ID         :" + cliente.Id);
+            streamWriter.WriteLine("Nome       :" + cliente.Nome);
+            streamWriter.WriteLine("Email      :" + cliente.Email);
+            streamWriter.WriteLine("CPF        :" + cliente.Cpf);
+            streamWriter.WriteLine("Telefone   :" + cliente.Telefone);
+            streamWriter.WriteLine("Incluido em:" + cliente.DataInclusao);
+            streamWriter.WriteLine("...");
+
+            streamWriter.Close();
+        }
+    }
+}
